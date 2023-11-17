a2be9932198fc42ace836fd302e705b1fadc3b94
kulgg <75735874+kulgg@users.noreply.github.com>
2023-11-17 14:04:29 +0100

    Add form

---
+++ app/InputForm.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  ip: z.string().min(8),
+});
+
+function InputForm() {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      ip: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    router.push(`/${values.ip}`);
+  }
+
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="ip"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>IP Address</FormLabel>
+                <FormControl>
+                  <Input placeholder="123.123.123.123" {...field} />
+                </FormControl>
+                <FormDescription>
+                  The IP address you want to geolocate
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
+
+export default InputForm;
